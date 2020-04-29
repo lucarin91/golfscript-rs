827e53e829faf25d28f79a09adc119c6c989856b
Luca Rinaldi <lucarin@protonmail.com>
2020-04-29 14:44:08 +0200

    refactoring lexer with a new lex_variable function

--- src/lexer.rs
+++ src/lexer.rs
@@ -16,21 +16,36 @@ pub fn lex(input: &str) -> Result<Box<[Item]>, GSError> {
     Ok(tokens.into_boxed_slice())
 }
 
+fn lex_variable(chars: &mut CharStream) -> String {
+    let mut string = String::new();
+    loop {
+        match chars.peek() {
+            Some(ch) if ch.is_alphanumeric() || *ch == '_' => {
+                string.push(*ch);
+                chars.next();
+            }
+            Some(_) | None => break,
+        }
+    }
+    return string;
+}
+
 fn lex_item(mut chars: &mut CharStream) -> Option<Result<Item, GSError>> {
     loop {
-        let item = match chars.next() {
+        let item = match chars.peek() {
             Some('#') => {
+                chars.next();
                 while let Some(&ch) = chars.peek() {
                     chars.next();
                     if ch == '\n' {
                         break;
                     }
                 }
-
                 continue;
             }
 
             Some('"') => {
+                chars.next();
                 let mut string = String::new();
                 loop {
                     match chars.next() {
@@ -52,12 +67,12 @@ fn lex_item(mut chars: &mut CharStream) -> Option<Result<Item, GSError>> {
                         }
                     }
                 }
-
                 let string = string.replace("\\\\", "\\").replace("\\\"", "\"");
                 Item::Str(string)
             }
 
             Some('{') => {
+                chars.next();
                 let mut block_items = Vec::new();
                 loop {
                     match chars.peek() {
@@ -92,14 +107,13 @@ fn lex_item(mut chars: &mut CharStream) -> Option<Result<Item, GSError>> {
                         }
                     }
                 }
-
                 Item::Block(block_items.into_boxed_slice())
             }
 
-            Some(ch) if ch.is_digit(10) => {
+            Some(&ch) if ch.is_digit(10) => {
+                chars.next();
                 let mut num = String::new();
                 num.push(ch);
-
                 while let Some(&ch) = chars.peek() {
                     if ch.is_digit(10) {
                         num.push(ch);
@@ -108,61 +122,51 @@ fn lex_item(mut chars: &mut CharStream) -> Option<Result<Item, GSError>> {
                         break;
                     }
                 }
-
                 Item::Num(num.parse::<i64>().unwrap())
             }
 
             // If we encounter a '-' immediately followed by a number, this
             // is bound to the number instead of treated as an operator.
-            Some(ch) if ch == '-' => match chars.peek() {
-                Some(&ch) if ch.is_digit(10) => match lex_item(&mut chars) {
-                    Some(Ok(Item::Num(x))) => Item::Num(-x),
-                    Some(Err(e)) => return Some(Err(e)),
-                    _ => unreachable!(),
-                },
+            Some(ch) if *ch == '-' => {
+                chars.next();
+                match chars.peek() {
+                    Some(&ch) if ch.is_digit(10) => match lex_item(&mut chars) {
+                        Some(Ok(Item::Num(x))) => Item::Num(-x),
+                        Some(Err(e)) => return Some(Err(e)),
+                        _ => unreachable!(),
+                    },
 
-                _ => Item::Op('-'),
-            },
-
-            Some(ch) if ch.is_whitespace() => continue,
-            Some(':') => {
-                let mut string = String::new();
-                // TODO: dublicated create a function for this
-                loop {
-                    match chars.peek() {
-                        Some(ch) if ch.is_alphanumeric() || *ch == '_' => {
-                            string.push(*ch);
-                            chars.next();
-                        }
-                        Some(_) | None => break,
-                    }
+                    _ => Item::Op('-'),
                 }
-                if string.is_empty() {
+            }
+
+            Some(ch) if ch.is_whitespace() => {
+                chars.next();
+                continue;
+            }
+
+            Some(':') => {
+                chars.next();
+                let var = lex_variable(chars);
+                if var.is_empty() {
                     return Some(Err(GSError::Parse(
                         "empty variable name after :".to_string(),
                     )));
                 }
-                Item::Assign(string)
+                Item::Assign(var)
             }
-            Some(ch @ '+') | Some(ch @ '-') | Some(ch @ '!') | Some(ch @ '@') | Some(ch @ '$')
-            | Some(ch @ '*') | Some(ch @ '/') | Some(ch @ '%') | Some(ch @ '|')
-            | Some(ch @ '&') | Some(ch @ '^') | Some(ch @ '\\') | Some(ch @ ';')
-            | Some(ch @ '<') | Some(ch @ '>') | Some(ch @ '=') | Some(ch @ '.')
-            | Some(ch @ '?') | Some(ch @ '(') | Some(ch @ ')') | Some(ch @ '[')
-            | Some(ch @ ']') | Some(ch @ '~') | Some(ch @ '`') | Some(ch @ ',') => Item::Op(ch),
-            Some(ch) => {
-                let mut string = ch.to_string();
-                // TODO: dublicated create a function for this
-                loop {
-                    match chars.peek() {
-                        Some(ch) if ch.is_alphanumeric() || *ch == '_' => {
-                            string.push(*ch);
-                            chars.next();
-                        }
-                        Some(_) | None => break,
-                    }
-                }
-                Item::Var(string)
+
+            Some('+') | Some('-') | Some('!') | Some('@') | Some('$') | Some('*') | Some('/')
+            | Some('%') | Some('|') | Some('&') | Some('^') | Some('\\') | Some(';')
+            | Some('<') | Some('>') | Some('=') | Some('.') | Some('?') | Some('(') | Some(')')
+            | Some('[') | Some(']') | Some('~') | Some('`') | Some(',') => {
+                Item::Op(chars.next().unwrap())
+            }
+
+            Some(_) => {
+                let var = lex_variable(chars);
+                // Varible could be empty?
+                Item::Var(var)
             }
 
             _ => return None,
